91f181afd4de44a2f843d38f24536e82740ab4b4
Daniel silva <89894235+Danielsilva200319@users.noreply.github.com>
2023-10-30 12:53:16 -0500

    Update Program.cs

--- ApiAnimals/Program.cs
+++ ApiAnimals/Program.cs
@@ -13,7 +13,6 @@ builder.Services.ConfigureRateLimiting();
 builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());
 builder.Services.ConfigureCors();
 builder.Services.AddApplicationServices();
-// builder.Services.AddAplicationServices();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
